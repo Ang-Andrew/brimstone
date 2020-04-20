715f3e8124c100da20046e816343aa1a77552cf8
Ang-Andrew <andrewang321@gmail.com>
2020-04-20 21:53:37 +1000

    intial commit, added program counter, register file and alu

---
+++ gateware/alu.v
@@ -0,0 +1,30 @@
+module register_file#(
+  parameter DATA_WIDTH_P = 32,
+  parameter ADDR_WIDTH_P = 5,
+  parameter CNTRL_WIDTH_P = 3
+)(
+  input wire clk,
+  input wire reset,
+  input wire [CNTRL_WIDTH_P-1:0] i_control,
+  input wire [DATA_WIDTH_P-1:0] i_a,
+  input wire [DATA_WIDTH_P-1:0] i_b,
+  output wire [DATA_WIDTH_P-1:0] o_result
+);
+  localparam[2:0]
+    OP_ADD  : 3'b010,
+    OP_SUB  : 3'b110,
+    OP_AND  : 3'b000,
+    OP_OR   : 3'b001
+
+  // operation decoder
+  always @(i_control) begin
+    case(i_control)
+      OP_ADD  : o_result = i_a + i_b; // add
+      OP_SUB  : o_result = i_a - i_b; // sub
+      OP_AND  : o_result = i_a & i_b; // AND
+      OP_OR  : o_result = i_a | i_b; // OR
+      default : o_result = {DATA_WIDTH_P{1'bx}}
+    endcase
+  end
+
+endmodule
---
+++ gateware/program_counter.v
@@ -0,0 +1,22 @@
+module program_counter#(
+  parameter DATA_WIDTH_P = 32
+)(
+  input wire clk,
+  input wire reset,
+  input wire [DATA_WIDTH_P-1:0] i_count_next
+  output wire [DATA_WIDTH_P-1:0] o_count
+);
+
+  assign o_count = next_count;
+
+  // counter process
+  always @(posedge clk) begin
+    data_out_valid <= 1'b0;
+    if (i_reset) begin
+      next_count <= {DATA_WIDTH_P{1'b0}};
+    end else begin
+      next_count <= i_count_next + 1;
+    end;
+  end
+
+endmodule
